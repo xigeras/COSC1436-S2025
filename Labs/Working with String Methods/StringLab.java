e6ab2c60f2a9b968ae31a0c06c01bea0296bf439
august b. <69170271+xigeras@users.noreply.github.com>
2025-06-11 11:37:17 +0000

    extracted and printed the word "java" from the stringLab variable using substring method

--- Labs/Working with String Methods/StringLab.java
+++ Labs/Working with String Methods/StringLab.java
@@ -1,10 +1,12 @@
 public class StringLab
 {
+    static String stringLab = " Welcome to the Java String Lab! ";
     public static void main(String[] args)
     {
-        final String stringLab = "Welcome to the Java String Lab!";
+        
         System.out.println("The length of '" + stringLab + "' is " + stringLab.length() + " characters."); // length of the string
         System.out.println("The character at index 7 is: " + stringLab.charAt(7)); // character at index 7)
+        System.out.println("Substring of index 16 to 20 is: '" + stringLab.substring(16, 20) + "'"); // extracted and printed the word "Java" from the stringLab variable
     }
     public char charAt(int index) {
         return stringLab.charAt(index); // returns character at the specified index
