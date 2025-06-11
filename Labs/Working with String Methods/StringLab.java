347cee8f4236c31e14e5e9d03b73aee0f35c0d4e
august b. <69170271+xigeras@users.noreply.github.com>
2025-06-11 11:42:50 +0000

    used toUpperCase() and toLowerCase() methods to convert the string to uppercase and lowercase respectively, and printed the results. used indexOf() to find the index of the first occurrence of "Java" in the string, and printed it. used contains() to check if the string contains "Lab", and printed the result.

--- Labs/Working with String Methods/StringLab.java
+++ Labs/Working with String Methods/StringLab.java
@@ -7,6 +7,11 @@ public class StringLab
         System.out.println("The length of '" + stringLab + "' is " + stringLab.length() + " characters."); // length of the string
         System.out.println("The character at index 7 is: " + stringLab.charAt(7)); // character at index 7)
         System.out.println("Substring of index 16 to 20 is: '" + stringLab.substring(16, 20) + "'"); // extracted and printed the word "Java" from the stringLab variable
+        System.out.println("The uppercase version of stringLab is: '" + stringLab.toUpperCase() + "'"); // converted the string to uppercase
+        System.out.println("The lowercase version of stringLab is: '" + stringLab.toLowerCase() + "'"); // converted the string to lowercase
+        System.out.println("The index of the first occurence of 'Java' is: " + stringLab.indexOf("Java")); // index of the first occurrence of "Java"
+        System.out.println("Does the string contain 'Lab'? " + stringLab.contains("Lab"));
+        
     }
     public char charAt(int index) {
         return stringLab.charAt(index); // returns character at the specified index
