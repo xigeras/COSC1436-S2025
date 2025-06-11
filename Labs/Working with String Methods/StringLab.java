e5381679ae6f8cef47b17ee9fe3e255d7f6aa7f8
august b. <69170271+xigeras@users.noreply.github.com>
2025-06-11 11:17:19 +0000

    Added a length method to StringLab.java to calculate the character length of stringLab.

--- Labs/Working with String Methods/StringLab.java
+++ Labs/Working with String Methods/StringLab.java
@@ -2,6 +2,9 @@ public class StringLab
 {
     public static void main(String[] args)
     {
-        
+        final String stringLab = "Welcome to the Java String Lab!";
+        System.out.println("The length of '" + stringLab + "' is " + stringLab.length() + " characters."); // length of the string
+        // System.out.println("The character at index 7 within '" + stringLab + "' is '" + str.charAt(stringLab, 7)); // character at index 7
+
     }
 }
