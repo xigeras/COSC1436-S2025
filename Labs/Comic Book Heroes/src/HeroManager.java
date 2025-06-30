f0ab14a65ddf24328257b724f7cd25729d021c26
august b. <69170271+xigeras@users.noreply.github.com>
2025-06-30 03:06:56 +0000

    add and remove hero methods been made

--- Labs/Comic Book Heroes/src/HeroManager.java
+++ Labs/Comic Book Heroes/src/HeroManager.java
@@ -3,4 +3,20 @@ import java.util.ArrayList;
 
 public class HeroManager {
     public ArrayList<Hero> heroes = new ArrayList<Hero>();
+
+    // adds a hero
+    public void addHero(Hero hero) {
+        heroes.add(hero);
+    }
+
+    // removes a hero
+    public void removeHero(String name) {
+        heroes.removeIf(hero -> hero.getName().equals(name));
+    }
+
+    // Sorting (BubbleSort and InsertionSort)!!
+
+
+    // 2D array Display
+
 }
