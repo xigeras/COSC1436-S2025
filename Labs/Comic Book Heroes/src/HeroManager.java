583fc0192540519cdd69c7744cb0ae8f6b9507f0
august b. <69170271+xigeras@users.noreply.github.com>
2025-06-30 03:10:02 +0000

    bubble sort by ascending done. used StudentManager from notes to help

--- Labs/Comic Book Heroes/src/HeroManager.java
+++ Labs/Comic Book Heroes/src/HeroManager.java
@@ -14,8 +14,21 @@ public class HeroManager {
         heroes.removeIf(hero -> hero.getName().equals(name));
     }
 
-    // Sorting (BubbleSort and InsertionSort)!!
+    // Sorting (BubbleSort and InsertionSort)!! in ascending order
 
+    public void bubbleSortByPowerLevel() {
+        for (int i = 0; i < heroes.size() - 1; i++) {
+            for (int j = 0; j < heroes.size() - i - 1; j++) {
+                if(heroes.get(j).getLevel() > students.get(j + 1).getLevel() {
+                    Hero temp = heroes.get(j);
+                    heroes.set(j, heroes.get(j + 1));
+                    heroes.set(j + 1, temp);
+                }
+            }
+        }
+    }
+
+    
 
     // 2D array Display
 
